62c85a9d23e1ac6f16e7f3c12318043b101ee0f9
Kevin Guebert <kguebert93@gmail.com>
2021-08-12 07:21:17 -0400

    Bugfix

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+
+module.exports = withNextra({
+  webpack(config) {
+    config.plugins.pop() // remove stork plugin which was added by nextra. Remove when nextra version is bumped!
+    return config
+  }
+})
 module.exports = withNextra()
