a27898c10134250a72ca938ed23f51b5880ab1fe
Kevin Guebert <kevin@wattch.io>
2022-11-28 06:56:56 -0500

    Fix imports

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-
-module.exports = withNextra({
-  webpack(config) {
-    config.plugins.pop() // remove stork plugin which was added by nextra. Remove when nextra version is bumped!
-    return config
-  }
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true
 })
+
+module.exports = withNextra({})
